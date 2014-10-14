ad18ad5e5d3ed3fbe54525ae3defdaf0b57cd225
gonzalez2 <cgonzalez_912@yahoo.com>
2014-10-13 18:13:36 -0700

    Added classes

--- src/iClicker.java
+++ src/iClicker.java
@@ -1,9 +1,8 @@
-
+import java.util.*;
 public class iClicker {
 
 	public static void main(String[] args) {
-		System.out.println("Starting pj 1");
-		System.out.println("update github");
+		
 
 	}
 
